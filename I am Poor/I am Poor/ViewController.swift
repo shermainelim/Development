e1c912f8ed0f051bf8be348eb358b58b635144de
shermainelim <shermaine.lim.si.hui@gmail.com>
2022-01-12 20:17:54 +0800

    Swift Storyboarding 1 (Rich/Poor)

---
+++ I am Poor/I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Shermaine Lim on 12/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
